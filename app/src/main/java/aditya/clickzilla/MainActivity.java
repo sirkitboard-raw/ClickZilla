bad4d1644104bd00d81a96094c2b819186824553
Adity Balwani <adibalwani@gmail.com>
2014-07-13 21:13:04 +0530

    Removed Options Menu

--- app/src/main/java/aditya/clickzilla/MainActivity.java
+++ app/src/main/java/aditya/clickzilla/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        //getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -33,9 +33,9 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
         return super.onOptionsItemSelected(item);
     }
 }
