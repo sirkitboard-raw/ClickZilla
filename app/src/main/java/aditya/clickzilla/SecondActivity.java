24159ef94dfab9095c1cd2267a3e8b25486f018c
Adity Balwani <adibalwani@gmail.com>
2014-07-13 22:20:51 +0530

    Quest 4

--- app/src/main/java/aditya/clickzilla/SecondActivity.java
+++ app/src/main/java/aditya/clickzilla/SecondActivity.java
@@ -1,30 +1,30 @@
 package aditya.clickzilla;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import aditya.clickzilla.R;
 
-public class MainActivity extends Activity {
-	public int counter;
+public class SecondActivity extends Activity {
+	int counter;
 	Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-	    toast = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT);
-	    counter = 0;
+        setContentView(R.layout.activity_second);
+	    counter = getIntent().getIntExtra("clicks",0);
+	    toast = Toast.makeText(getApplicationContext(),"",Toast.LENGTH_SHORT);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.second, menu);
         return true;
     }
 
@@ -46,11 +46,5 @@ public class MainActivity extends Activity {
 		counter++;
 		toast.setText("Number of clicks : "+counter);
 		toast.show();
-	}
-	public void launchNext(View view) {
-		Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
-		intent.putExtra("clicks",counter);
-		startActivity(intent);
-		finish();
 	}
 }
